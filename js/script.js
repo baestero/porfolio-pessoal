2c70c0ecd621347dc1f0c9b1dbb94873066d0783
Leonardo Baestero <leonardobaestero@gmail.com>
2023-12-16 12:07:59 -0300

    adiçãos de animações

--- js/script.js
+++ js/script.js
@@ -1,10 +1,30 @@
 function menuShow() {
   let menuMobile = document.querySelector(".mobile-menu");
+  let icon = document.querySelector(".icon");
+
   if (menuMobile.classList.contains("open")) {
     menuMobile.classList.remove("open");
-    document.querySelector(".icon").src = "./icons/menu_white_36dp.svg";
+    icon.style.transform = "rotate(0deg)"; // Rotação de volta para a posição inicial (se necessário)
+    setTimeout(() => {
+      icon.src = "./icons/menu_white_36dp.svg"; // Altera a imagem do ícone
+    }, 300); // Tempo correspondente à transição CSS (0.3s)
   } else {
     menuMobile.classList.add("open");
-    document.querySelector(".icon").src = "./icons/close_white_36dp.svg";
+    icon.style.transform = "rotate(180deg)"; // Rotação (se desejado)
+    setTimeout(() => {
+      icon.src = "./icons/close_white_36dp.svg"; // Altera a imagem do ícone
+    }, 300); // Tempo correspondente à transição CSS (0.3s)
   }
 }
+
+const animacao = document.querySelectorAll(".js-scroll");
+
+animacao.forEach((item) => {
+  item.classList.add("ativo1");
+});
+
+const animacaoMenu = document.querySelectorAll(".js-scroll-menu");
+
+animacaoMenu.forEach((item) => {
+  item.classList.add("ativo2");
+});
